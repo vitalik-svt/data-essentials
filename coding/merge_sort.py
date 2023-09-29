cedab8a2b6919d96222cdc54fef2a5b4dc758e9f
notsovitalik <zederblatt@gmail.com>
2023-09-29 18:12:08 +0400

    add a bit of coding

---
+++ coding/merge_sort.py
@@ -0,0 +1,17 @@
+import unittest
+from collections.abc import Iterable
+
+class Sorting:
+
+	def __init__(self, values: Iterable):
+		self.values = values
+
+	def merge_sort(self):
+
+		len = len(self.values)
+
+	def merge(self, )
+
+
+	def __repr__(self):
+		return str(self.values)
