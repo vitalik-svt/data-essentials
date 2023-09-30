22d02c54a7c394a40cc5817f321734395d60eed8
notsovitalik <zederblatt@gmail.com>
2023-09-30 10:47:25 +0400

    add some

--- coding/merge_sort.py
+++ coding/merge_sort.py
@@ -3,14 +3,19 @@ from collections.abc import Iterable
 
 class Sorting:
 
+	'''implementation of merge sorting algorithm, that takes iterable and sort it'''
+
 	def __init__(self, values: Iterable):
 		self.values = values
+
 
 	def merge_sort(self):
 
 		len = len(self.values)
 
-	def merge(self, )
+
+	def merge(self):
+		pass
 
 
 	def __repr__(self):
